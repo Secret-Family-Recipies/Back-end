c3051f4e083ce1f14ab9c932c5827d69d990181b
Caleb Smith <52614744+Claeb1mb@users.noreply.github.com>
2021-02-26 22:11:52 -0500

    Revert "initial commit"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
+// server file here
+require("dotenv").config()
 
-const server = require("./api/server");
+const server = require("./api/server")
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`server running on ${port}`));
+server.listen(port, () => console.log(`server running on ${port}`))
