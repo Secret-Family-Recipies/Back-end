b0ec7ebdda0ee200809d6da7d5fc58d889c0ce98
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2021-03-02 23:36:13 -0500

    db congif

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,6 @@
 const knex = require("knex");
-
 const knexfile = require("../knexfile.js");
-const env = process.env.NODE_ENV || "development";
 
 
-module.exports = knex(knexfile[env]);
+
+module.exports = knex(knexfile.development);
